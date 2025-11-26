72313c415306812fc00d0b050484ee4d99773fe1
Xadicha Mamadaliyeva <xmamadaliyeva@gmail.com>
2025-11-26 09:13:15 +0500

    Ozgarishlar qoshildi

--- blog/models.py
+++ blog/models.py
@@ -1,60 +1,61 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(verbose_name="Category name",max_length=250)
+    name = models.CharField(verbose_name="Category name", max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
-    
+
     def __str__(self):
         return str(self.name)
-print("hello world")
-    
+
+
 class Tag(models.Model):
-    name = models.CharField(verbose_name="Tag name",max_length=250)
+    name = models.CharField(verbose_name="Tag name", max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
-    
+
     def __str__(self):
         return str(self.name)
 
 
 class Post(models.Model):
-    title = models.CharField(verbose_name="Post title",max_length=550)
+    title = models.CharField(verbose_name="Post title", max_length=550)
     body = models.TextField(verbose_name="Post body")
-    author = models.CharField(verbose_name="Post author",default="Admin",max_length=100)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT,related_name='posts')
+    author = models.CharField(verbose_name="Post author", default="Admin", max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
     tag = models.ManyToManyField(Tag)
     views = models.PositiveIntegerField(default=0)
-    publish_date = models.DateTimeField(verbose_name="Published time",auto_now_add=True)
+    publish_date = models.DateTimeField(verbose_name="Published time", auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)  # 🔥 Kichik o‘zgarish (qo‘shildi)
+
     published = models.BooleanField(default=True)
     on_top = models.BooleanField(default=False)
-    
-    
+
     def get_avg_rating(self):
-        # print([int(i.value) for i in self.ratings.all()])
         sum_ratings = sum([int(i.value) for i in self.ratings.all()])
         try:
-            return round(sum_ratings / self.ratings.all().count(),2)
-        except Exception as e:
-            return 0     
-    
+            return round(sum_ratings / self.ratings.count(), 2)
+        except Exception:
+            return 0
+
     def __str__(self):
         return str(self.title)
-    
+
 
 class Comment(models.Model):
-    author = models.CharField(verbose_name="Comment author",max_length=100,blank=False)
+    author = models.CharField(verbose_name="Comment author", max_length=100, blank=False)
     comment = models.TextField(verbose_name="Comment")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    
-    
+
     def __str__(self):
         return str(self.author)
-    
-    
+
+
 class Rating(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings')
-    value = models.PositiveSmallIntegerField(verbose_name="Post rating",default=0)
-    
+    value = models.PositiveSmallIntegerField(verbose_name="Post rating", default=0)
+
     def __str__(self):
         return str(self.value)
